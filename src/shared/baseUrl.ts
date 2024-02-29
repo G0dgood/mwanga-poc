a255d97b67814fe0676e641679f578afdad9fecf
user <chinedu.go@gmail.com>
2024-02-29 08:50:06 +0100

    changed the base URL

--- src/shared/baseUrl.ts
+++ src/shared/baseUrl.ts
@@ -1,8 +1,7 @@
-// export const baseUrl = "https://outcesscrmapi.herokuapp.com";
-   // export const baseUrl = "https://105.112.129.219:443";
 
-   export const baseUrl = "http://localhost:5000";
-// export const baseUrl = "https://crm.outcess.com:443"; 
+ export const baseUrl = "https://mwanga.ptlsoftwaresolutions.ng:443"
+
+  //  export const baseUrl = "http://localhost:5000"; 
 
 
 export const buildDynamicURL = (id: any, fromDate: any, toDate: any, limit: any, page: any, base: any) => {
